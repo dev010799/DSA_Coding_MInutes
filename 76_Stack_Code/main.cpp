88f5afa920cbaa25327adbef2f9ea1d115d20d1b
Dev Mishra <dev010799@gmail.com>
2023-10-05 01:53:13 +1200

    Delete Middle element from stack

--- 76_Stack_Code/main.cpp
+++ 76_Stack_Code/main.cpp
@@ -196,9 +196,6 @@ public:
     }
 };
 
-*/
-
-
 // <********************************************************************************************>
 
 // Question: Reverse a string using stack.
@@ -224,6 +221,27 @@ int main() {
 }
 
 // Output : arhsiMveD
-
+*/
 
 // <********************************************************************************************>
+// Question: Delete Middle Element from stack.
+
+void solve(stack<int> &inputStack, int count, int size) {
+    // Base Case
+    if(count = size/2) {
+        inputStack.pop();
+        return;
+    }
+    int num = inputStack.top();
+    inputStack.pop();
+
+    // Recursive Call
+    solve(inputStack, count + 1, size);
+
+    inputStack.push(num);
+}
+
+void deleteMiddle(stack<int> &inputStack, int N) {
+    int count = 0;
+    solve(inputStack, count, N);
+}
