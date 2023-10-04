1090dc07b9cd3ea48da617011e5e7c0a0aae4672
Dev Mishra <dev010799@gmail.com>
2023-10-05 00:10:29 +1200

    Stack operations using class

--- 76_Stack_Code/main.cpp
+++ 76_Stack_Code/main.cpp
@@ -1,29 +1,138 @@
 #include<iostream>
-#include<stack>
+#include<Stack>
 using namespace std;
 
+class Stack {
+    // Properties
+    public: 
+        int *arr;
+        int top;
+        int size;
+
+    // Behaviour
+    Stack(int size) {
+        this -> size = size;
+        arr = new int[size];
+        top = -1;
+    }
+
+    void push(int element) {
+        if(size - top > 1) {
+            top++;
+            arr[top] = element;
+        }
+        else {
+            cout << "stack overflow" << endl;
+        }
+
+    }
+
+    void pop() {
+        if(top >= 0) {
+            top--;
+        }
+        else {
+            cout << "stack underflow" << endl;
+        }
+
+    }
+    
+    int peek() {
+        if(top >= 0 && top < size)
+        return arr[top];
+        else {
+            cout << "stack is empty" << endl;
+            return -1;
+        }
+
+    }
+
+    bool isEmpty() {
+        if(top == -1) {
+            return true;
+        }
+        else {
+            return false;
+        }
+
+    }
+
+
+};
+
 int main() {
-    // creation of stack
-    stack<int> s;
+
+    Stack st(5);
     
-    // Push Operation
-    s.push(2);
-    s.push(3);
+    st.push(22);
+    st.push(43);
+    st.push(44);
+    st.push(22);
+    st.push(43);
+    st.push(44);
 
-    // Pop Operation
-    s.pop();
+    cout << st.peek() << endl;
 
-    // Top/Peak operation
-    cout << "Printing top element: " << s.top() << endl;
+    st.pop();
+    cout << st.peek() << endl;
+
+     st.pop();
+    cout << st.peek() << endl;
     
-    // Isempty operation:
+     st.pop();
+    cout << st.peek() << endl;
 
-    if(s.empty()) {
-        cout << "Stack is empty " << endl;
+    st.push(789);
+    cout << st.peek() << endl;
+
+    if(st.isEmpty()) {
+        cout << "Stack is empty" << endl;
     }
     else {
-        cout << "Stack is fulfilled " << endl;
+        cout << "Stack is non-empty" << endl;
     }
+
+    st.pop(); 
+    cout << st.peek() << endl;
+
+    if (st.isEmpty()) {
+        cout << "Stack is empty" << endl;
+    }
+    else {
+        cout << "Stack is non-empty" << endl;
+    }
+     
+    
+    
+
+
+   
+
+
+
+
+
+    // // creation of stack
+    // stack<int> s;
+    
+    // // Push Operation
+    // s.push(2);
+    // s.push(3);
+
+    // // Pop Operation
+    // s.pop();
+
+    // // Top/Peak operation
+    // cout << "Printing top element: " << s.top() << endl;
+    
+    // // Isempty operation:
+
+    // if(s.empty()) {
+    //     cout << "Stack is empty " << endl;
+    // }
+    // else {
+    //     cout << "Stack is fulfilled " << endl;
+    // }
 
     return 0;
 }
