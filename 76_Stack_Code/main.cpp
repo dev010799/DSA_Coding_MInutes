2fd052ecdb5ffb3cdfd095834d2e0896b3c7aaee
Dev Mishra <dev010799@gmail.com>
2023-10-07 03:35:48 +1200

    Redundant brackets proble

--- 76_Stack_Code/main.cpp
+++ 76_Stack_Code/main.cpp
@@ -341,7 +341,6 @@ void reverseStack(stack<int> &stack) {
 
     // insertAtBottom(stack, element);
 }
-*/
 
 // <********************************************************************************************>
 // Question: Sort a Stack
@@ -372,4 +371,36 @@ void sortStack(stack<int> &stack) {
     // Recursive Case:
     sortStack(stack);
     // sortedInsert(stack, num);
+}
+*/
+// <********************************************************************************************>
+// Question: Redundant Brackets.
+bool findRedundantBrackets(string &s) {
+    stack<char> st;
+    for(int i = 0; i < s.length(); i++) {
+        char ch = s[i];
+
+        if(ch == '(' || ch == '+' || ch == '*' || ch == '/') {
+            st.push(ch);
+        }
+        else {
+            // ch is ')' or lower case letter
+            if(ch == ')') {
+                bool isRedundant = true;
+                while(st.top() != '(') {
+                    char top = st.top();
+                    if(top == '(' || top == '+' || top == '*' || top == '/') {
+                        isRedundant = false;
+                    }
+                    st.pop();
+                }
+                if(isRedundant == true) 
+                    return true;
+                st.pop();
+            }
+
+        }
+
+    }
+    return false;
 }
