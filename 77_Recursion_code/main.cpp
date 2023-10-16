88d3643b88a9c945753180dfa34e2aab30c44d7b
Dev Mishra <dev010799@gmail.com>
2023-10-17 04:11:06 +1200

    build two functions for rat in a maze

--- 77_Recursion_code/main.cpp
+++ 77_Recursion_code/main.cpp
@@ -1,3 +1,4 @@
+/*
 #include<iostream>
 using namespace std;
 
@@ -19,3 +20,41 @@ int main() {
     cout << ans << endl;
     return 0;
 }
+*/
+
+// Rat in a Maze Problem:
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// Maze Size
+#define N 4
+
+bool solveMazeUtil(int maze[N][N], int x, int y, int sol[N][N]);
+
+// A utility function to print solution matrix sol[N][N]
+void printSolution(int sol[N][N]) 
+{
+    for(int i = 0; i < N; i++) {
+        for(int j = 0; j < N; j++)
+            cout << " " << sol[i][j] << " ";
+        cout << endl;
+    }
+}
+
+// A utility function to check if x, y is valid index for
+// N*N maze
+
+bool isSafe(int maze[N][N], int x, int y) 
+{
+    // if(x, y outside maze) return false
+    if(x >= 0 && x < N && y >= 0 && y < N && maze[x][y] ==  1)
+        return true;
+    return false;
+}
+
+
+
+
+
+
