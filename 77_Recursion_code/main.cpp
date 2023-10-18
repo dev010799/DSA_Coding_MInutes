86b3c3114eba20a3767b569e450c77b8083dc284
Dev Mishra <dev010799@gmail.com>
2023-10-18 18:43:43 +1200

    Rat and maze problem solved using recursion

--- 77_Recursion_code/main.cpp
+++ 77_Recursion_code/main.cpp
@@ -53,6 +53,73 @@ bool isSafe(int maze[N][N], int x, int y)
     return false;
 }
 
+bool solveMaze(int maze[N][N])
+{
+    int sol[N][N] = { {0, 0, 0, 0},
+                      {0, 0, 0, 0},
+                      {0, 0, 0, 0},
+                      {0, 0, 0, 0}
+                    };
+    if(solveMazeUtil(maze, 0, 0, sol) == false) {
+        cout << "Solution doesn't exist";
+        return false;
+    }
+    printSolution(sol);
+    return true;
+}
+
+// A recursive Utility function to solve maze problem
+bool solveMazeUtil(int maze[N][N], int x, int y, int sol[N][N]) {
+    if(x == N - 1 && y == N - 1 && maze[x][y] == 1) {
+        sol[x][y] = 1;
+        return true;
+    }
+    // check if maze[x][y] is valid 
+    if(isSafe(maze, x, y) == true) {
+        if(sol[x][y] == 1)
+            return false;
+    // mark x, y as part of solution path
+    sol[x][y] = 1;
+
+    // Move forward in x direction
+    if(solveMazeUtil(maze, x + 1, y, sol) == true)
+        return true;
+
+    // If moving right didnt work
+    // Move left
+    if(solveMazeUtil(maze, x - 1, y, sol) == true)
+        return true;
+    
+    // If moving in x direction doesn't give solution
+    // then move down in y direction
+    if(solveMazeUtil(maze, x, y + 1, sol) == true)
+        return true;
+
+    // If moving down didn't work 
+    // Move up
+    if(solveMazeUtil(maze, x, y - 1, sol) == true)
+        return true;
+    
+    if(solveMazeUtil(maze, x, y - 1, sol) == true) 
+        return true;
+    
+    // If none of the above movements work then
+    // BACKTRACK: unmark x, y as part of solution path
+    sol[x][y] = 0;
+    return false;
+    }
+    return false;
+}
+
+int main() {
+    int maze[N][N] = { {1, 0, 0, 0},
+                       {1, 1, 0, 1},
+                       {0, 1, 0, 0},
+                       {1, 1, 1, 1}};
+    solveMaze(maze);
+    return 0;
+}
+
 
 
 
